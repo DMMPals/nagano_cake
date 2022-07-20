3121495dc32eecfa7f107fe086957eaae51651cd
YuShimojo <yongxiatiao@gmail.com>
2022-07-20 07:14:45 +0000

    [Add]カラム

--- db/migrate/20220719061831_create_items.rb
+++ db/migrate/20220719061831_create_items.rb
@@ -1,6 +1,12 @@
 class CreateItems < ActiveRecord::Migration[6.1]
   def change
     create_table :items do |t|
+      t.string :name, null: false
+      t.integer :price, null: false
+      t.text :explan, null: false
+      t.string :image_id, null: false
+      t.boolean :is_active, default: true, null: false
+    
 
       t.timestamps
     end
