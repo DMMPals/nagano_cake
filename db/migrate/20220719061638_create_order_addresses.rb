f16c541f5c0c7aa75376df00ce6e2db2b511f3fc
YuShimojo <yongxiatiao@gmail.com>
2022-07-20 08:24:08 +0000

    [Fix]order_adress変更

--- db/migrate/20220719061638_create_order_addresses.rb
+++ db/migrate/20220719061638_create_order_addresses.rb
@@ -1,7 +1,10 @@
 class CreateOrderAddresses < ActiveRecord::Migration[6.1]
   def change
     create_table :order_addresses do |t|
-      t.references :customer_id, foreign_key: true
+      t.references :order_id, foreign_key: true
+      t.string :order_name, null: false
+      t.string :post, null: false
+      t.text :address, null: false
 
       t.timestamps
     end
