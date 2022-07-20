43ed323bafef2916e9c0b7ebf7568a784e1760d5
EC2 Default User <ec2-user@ip-172-31-0-21.ec2.internal>
2022-07-20 06:02:38 +0000

    framemake-controller

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,2 +1,20 @@
 class Public::CartItemsController < ApplicationController
+  
+  before_action :authenticate_customer!
+
+  def index
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  def create
+  end
+
 end
