7a37e8e139e97a03d306ae4621c4a5093f475a5f
EC2 Default User <ec2-user@ip-172-31-0-21.ec2.internal>
2022-07-20 07:32:31 +0000

    edit-migrate-oeder_addresses

--- db/migrate/20220719061638_create_order_addresses.rb
+++ db/migrate/20220719061638_create_order_addresses.rb
@@ -2,6 +2,10 @@ class CreateOrderAddresses < ActiveRecord::Migration[6.1]
   def change
     create_table :order_addresses do |t|
 
+      t.string :order_name, null: false
+      t.string :post, null: false
+      t.text :address, null: false
+
       t.timestamps
     end
   end
