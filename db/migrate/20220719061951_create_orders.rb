37e24f9f196e27efbda79d7064416e678a221079
MORITAKOUHEI <106665501+MORITAKOUHEI@users.noreply.github.com>
2022-07-20 05:57:36 +0000

    Add column

--- db/migrate/20220719061951_create_orders.rb
+++ db/migrate/20220719061951_create_orders.rb
@@ -1,6 +1,14 @@
 class CreateOrders < ActiveRecord::Migration[6.1]
   def change
     create_table :orders do |t|
+      t.integer :payment,       null: false
+      t.integer :postage,       null:: false
+      t.integer :payment_price, null: false
+      t.string  :post,          null: false
+      t.text    :address,       null: false
+      t.string  :name,          null: false
+      t.integer :order_status,  null: false
+      
 
       t.timestamps
     end
