9419046c49c62a3921ca81ca9b74dbde9a2c557f
YuShimojo <yongxiatiao@gmail.com>
2022-07-20 08:06:37 +0000

    [Fix]id追加

--- db/migrate/20220719061831_create_items.rb
+++ db/migrate/20220719061831_create_items.rb
@@ -1,7 +1,7 @@
 class CreateItems < ActiveRecord::Migration[6.1]
   def change
     create_table :items do |t|
-      t.references :genre, foreign_key: true
+      t.references :genre_id, foreign_key: true
       t.string :name, null: false
       t.integer :price, null: false
       t.text :explain, null: false
--- db/migrate/20220719062048_create_cart_items.rb
+++ db/migrate/20220719062048_create_cart_items.rb
@@ -1,8 +1,8 @@
 class CreateCartItems < ActiveRecord::Migration[6.1]
   def change
     create_table :cart_items do |t|
-      t.references :customer, foreign_key: true
-      t.references :item, foreign_key: true
+      t.references :customer_id, foreign_key: true
+      t.references :item_id, foreign_key: true
       t.integer :quantity
 
       t.timestamps
