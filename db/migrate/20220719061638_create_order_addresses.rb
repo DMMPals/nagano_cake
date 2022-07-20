c1247e2c5532b531ff118ea60fd523787646d2b2
YuShimojo <yongxiatiao@gmail.com>
2022-07-20 08:10:41 +0000

    [Fix]id追加

--- db/migrate/20220719061638_create_order_addresses.rb
+++ db/migrate/20220719061638_create_order_addresses.rb
@@ -1,6 +1,7 @@
 class CreateOrderAddresses < ActiveRecord::Migration[6.1]
   def change
     create_table :order_addresses do |t|
+      t.references :customer_id, foreign_key: true
 
       t.timestamps
     end
