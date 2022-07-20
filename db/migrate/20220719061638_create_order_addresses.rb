60bec196ccb383fd37f1ef7c80347d18f6a6cc7a
YuShimojo <yongxiatiao@gmail.com>
2022-07-20 08:39:50 +0000

    [Fix]外部キー変更

--- db/migrate/20220719061638_create_order_addresses.rb
+++ db/migrate/20220719061638_create_order_addresses.rb
@@ -1,7 +1,7 @@
 class CreateOrderAddresses < ActiveRecord::Migration[6.1]
   def change
     create_table :order_addresses do |t|
-      t.references :order_id, foreign_key: true
+      t.integer :customer_id, foreign_key: true
       t.string :order_name, null: false
       t.string :post, null: false
       t.text :address, null: false
