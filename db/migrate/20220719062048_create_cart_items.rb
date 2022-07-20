26ad2f35b54327082516e6004f25d429f0e9f33c
YuShimojo <yongxiatiao@gmail.com>
2022-07-20 07:59:43 +0000

    [Fix]外部キー設定

--- db/migrate/20220719062048_create_cart_items.rb
+++ db/migrate/20220719062048_create_cart_items.rb
@@ -1,6 +1,8 @@
 class CreateCartItems < ActiveRecord::Migration[6.1]
   def change
     create_table :cart_items do |t|
+      t.references :customer, foreign_key: true
+      t.references :item, foreign_key: true
       t.integer :quantity
 
       t.timestamps
