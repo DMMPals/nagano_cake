5df20513fd295a13b3e76b04c142a9e90c9c2813
YuShimojo <yongxiatiao@gmail.com>
2022-07-21 15:24:38 +0000

    [Add]商品新規登録機能

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,15 +1,22 @@
 class Admin::ItemsController < ApplicationController
-  before_action :authenticate_admin!
-  before_action :set_genres
-  
-  def new
-  end
+
   
   def index
     @items = Item.all
   end
+  
+  def new
+    @item = Item.new
+  end
 
   def create
+    @item = Item.new
+    if @item.save
+          redirect_to admin_items_path(@item.id)
+    else
+          render "admin/items/new"
+    end
+      
   end
 
   def show
