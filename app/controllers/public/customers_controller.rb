7f4f280607ab0171ef403db5537655747b431b01
Naoki-Takahasi <106652260+naoki-takahasi@users.noreply.github.com>
2022-07-21 07:54:53 +0000

    [Add]Devise-cord

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,18 +1,23 @@
 class Public::CustomersController < ApplicationController
-  
+
   def show
   end
-  
+
   def edit
   end
-  
+
   def update
   end
-  
+
   def unsubscribe
   end
-  
+
   def withdraw
   end
-  
+
+  private
+
+  def customers_params
+    params.require(:customers).permit(:surname, :name, :surname_kana, :name_kana, :post, :address, :tel)
+  end
 end
