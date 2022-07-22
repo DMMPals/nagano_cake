889cb363c78dc0fd09fc856799d918a688547413
YuShimojo <yongxiatiao@gmail.com>
2022-07-22 14:15:40 +0000

    [Add]モデル記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,10 @@
 class Item < ApplicationRecord
-    
+  belongs_to :genre
+  
+  validates :image_id, presence: true
+  validates :name, presence: true
+  validates :explain, presence: true
+  validates :genre_id, presence: true
 #消費税を加えた商品価格のメソッド
     def add_tax_price
         (self.price * 1.10).round
