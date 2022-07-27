9d598bf9faf733d9de7c8c345d70806d974bfc6e
EC2 Default User <ec2-user@ip-172-31-0-21.ec2.internal>
2022-07-27 07:48:45 +0000

    マージ調整

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,9 +1,19 @@
 class Admin::OrdersController < ApplicationController
 
+  def index
+    @orders = Order.all
+  end
+
   def show
+    @order = Order.find(params[:id])
+    @order_items = OrderItem.where(order_id: params[:id])
   end
 
   def update
   end
 
+
+  def order_params
+    params.require(:order).permit(:order_status)
+  end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -4,4 +4,8 @@ class Order < ApplicationRecord
 
   enum payment: { credit_card: 0, transfer: 1 }
 
+  #入金待ち:0, 入金確認:1, 製作中:2, 発送準備中:3, 発送済み:4
+  enum order_status: { waiting_pay: 0, confirm_payment: 1, working:2, ready_to_ship:3, sent:4 }
+
+
 end
