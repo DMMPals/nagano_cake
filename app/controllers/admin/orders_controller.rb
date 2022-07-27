d22ee83477cbc04a266d7db4c0af51343620cbf3
EC2 Default User <ec2-user@ip-172-31-0-21.ec2.internal>
2022-07-27 08:19:42 +0000

    ヘッダー取り込み

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,9 +1,5 @@
 class Admin::OrdersController < ApplicationController
 
-  def index
-    @orders = Order.all
-  end
-
   def show
     @order = Order.find(params[:id])
     @order_items = OrderItem.where(order_id: params[:id])
