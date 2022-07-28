a67b3afe632e5bc5332c4d7a1478c9febe9cd320
MORITAKOUHEI <106665501+MORITAKOUHEI@users.noreply.github.com>
2022-07-28 07:02:40 +0000

    order_index

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,7 @@
 class Order < ApplicationRecord
   has_many :order_details, dependent: :destroy
   belongs_to :customer
-
+  has_many :items,through: :order_details
   enum payment: { credit_card: 0, transfer: 1 }
-
+  enum order_status: { no_payment: 0, confirm_payment: 2, production: 3, ready_to_ship: 4, sent: 5 }
 end
